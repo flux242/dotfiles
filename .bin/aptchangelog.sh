13893fa473f35e2d9f518239e23ae14853e2c7ed
alex <alex@chrome>
2018-02-19 11:58:26 +0100

    BUGFIX: removing 'digit:' from version string now

--- .bin/aptchangelog.sh
+++ .bin/aptchangelog.sh
@@ -11,13 +11,19 @@ upgradable=$(apt list --upgradable 2>/dev/null | awk -F/ '/[^\/]*\//{print $1}')
 for package in ${upgradable[@]}; do
   version=$(dpkg -l "$package" | awk -F\  '/^ii/{print $3}' | head -n1)
   [[ -n "$version" ]] && {
+    # Ubuntu packagers could prepend a package version with a 'digit:'
+    # For example for bsdutils version is '1:2.30.1-0ubuntu4' but internally
+    # they have util-linux package with 2.30.1-ubuntu4. Whatever they need it
+    # for but it should be removed first
+    version="${version/[[:digit:]]:/}"
     # some characters in the version string should be escaped. Such
     # chars have special meaning in regular expressions. Currently
     # only the '+' char is escaped but God only knows what other chars
     # these freak&fucks could use in the vesion string 
     version="${version/+/\\\\+}" # plus should be prepended with \\ for awk
+    echo "$version"
     echo -e "${LIGHTRED}Package: ${LIGHTGREEN}${package}${NC}"
-    apt-get -q changelog "$package" | awk -v ver="$version" '{if($0 ~ "[^(]*\\("ver"\\).*"){found=1};if(found!=1)print $0}'
+    apt-get -q changelog "$package" | awk -v ver="$version" '{if($0 ~ "[^(]*\\(.*"ver"\\).*"){found=1};if(found!=1)print $0}'
   }
 done
 
