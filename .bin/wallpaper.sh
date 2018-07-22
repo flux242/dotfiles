0022fe439e72df8cbf607923859fed8265963999
alex <alex@chrome>
2018-07-22 15:00:51 +0200

    CHANGED: checking if feh or hsetroot is installed

--- .bin/wallpaper.sh
+++ .bin/wallpaper.sh
@@ -5,4 +5,5 @@ wdir=${1:-~/wallpapers}
 [[ -d "$wdir" ]] || wdir=/usr/share/xfce4/backdrops/
 TIFS=$IFS;IFS=$'\n';warr=( $(ls "$wdir") );IFS=$TIFS
 wname=$(/usr/bin/yad --entry --title "Available wallpapers" --text "Choose wallpaper image" --entry-text "${warr[@]}" 2>/dev/null)
-[[ -n "$wname" ]] && feh --bg-scale "$(find "${wdir}" -iname "$wname"|shuf|head -n1)"
+[[ -n "$(which feh)" ]] && [[ -n "$wname" ]] && feh --bg-scale "$(find "${wdir}" -iname "$wname")"
+[[ -n "$(which hsetroot)" ]] && [[ -n "$wname" ]] && hsetroot -fill "$(find "${wdir}" -iname "$wname")"
