73ffd1073574ae3f47dd397a02ce8e814dd6c83c
alex <alex@chrome>
2018-12-10 07:14:48 +0100

    CHANGED: dmesg output added to the layout

--- .i3/load_layout.sh
+++ .i3/load_layout.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-i3-msg "workspace 1; append_layout ~/.i3/workspace1.json"
+i3-msg "workspace 1; append_layout ~/.i3/workspace2.json"
 
 (x-terminal-emulator -T TerminalTailSyslog -e 'env PROMPT_COMMAND="unset PROMPT_COMMAND; tail -F /var/log/syslog" bash') &
+(x-terminal-emulator -T TerminalDmesg -e 'env PROMPT_COMMAND="unset PROMPT_COMMAND; dmesg -wt" bash') &
 (x-terminal-emulator -T TerminalShownetstat -e 'env PROMPT_COMMAND="unset PROMPT_COMMAND; shownetstat" bash') &
 (x-terminal-emulator -T TerminalWork -e 'bash') &
 
