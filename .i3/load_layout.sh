e6bd7a510cf03e45fe72239e08441d258d3d88b6
alex <alex@ubu1804.lan>
2019-02-13 12:21:38 +0000

    CHANGED: i3 load_layout made compatible with the urxvt

--- .i3/load_layout.sh
+++ .i3/load_layout.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+# Note: -title option is important and is proven to be compatible with:
+#        - xfce4-terminal ( through the xfce4-terminal-wrapper
+#        - urxvt        
+
 i3-msg "workspace 1; append_layout ~/.i3/workspace2.json"
 
-(x-terminal-emulator -T TerminalTailSyslog -e 'env PROMPT_COMMAND="unset PROMPT_COMMAND; tail -F /var/log/syslog" bash') &
-(x-terminal-emulator -T TerminalDmesg -e 'env PROMPT_COMMAND="unset PROMPT_COMMAND; dmesg -wt" bash') &
-(x-terminal-emulator -T TerminalShownetstat -e 'env PROMPT_COMMAND="unset PROMPT_COMMAND; shownetstat" bash') &
-(x-terminal-emulator -T TerminalWork -e 'bash') &
+(x-terminal-emulator -title TerminalTailSyslog -e sh -c 'env PROMPT_COMMAND="unset PROMPT_COMMAND; tail -F /var/log/syslog" bash') &
+(x-terminal-emulator -title TerminalDmesg -e sh -c 'env PROMPT_COMMAND="unset PROMPT_COMMAND; dmesg -wt" bash') &
+(x-terminal-emulator -title TerminalShownetstat -e sh -c 'env PROMPT_COMMAND="unset PROMPT_COMMAND; shownetstat" bash') &
+(x-terminal-emulator -title TerminalWork -e 'bash') &
 
