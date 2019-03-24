175d9960418e7b469677ba4a9e8316add80ac21f
alex <alex@email>
2019-03-24 12:44:20 +0100

    CHANGED: start_appimage creates privatehome dir automatically

--- .bin/start_appimage.sh
+++ .bin/start_appimage.sh
@@ -5,7 +5,7 @@
 appbin=$(find ~/Applications/ -iname "$APP_NAME"'*' -exec sh -c '[ -x "{}" -a ! -d "{}" ] && echo "{}"' \;)
 if [ -n "$appbin" ]; then
   if [ -n "$(which firejail)" ]; then
-    [ -d ~/Applications/privatehome/"$APP_NAME" ] || mkdir ~/Applications/privatehome/"$APP_NAME"
+    [ -d ~/Applications/privatehome/"$APP_NAME" ] || mkdir -p ~/Applications/privatehome/"$APP_NAME"
     profilename="${HOME}/.config/firejail/${APP_NAME}.profile"
     [ -s "$profilename" ] || profilename="/etc/firejail/${APP_NAME}.profile"
     [ -s "$profilename" ] || profilename='/etc/firejail/default.profile'
