458126eeb1786d66694c298f29adc685c911c792
alex <alex@chrome>
2019-05-01 11:18:31 +0200

    configured private home for the apps

--- .bin/start_appimage.sh
+++ .bin/start_appimage.sh
@@ -9,8 +9,11 @@ if [ -n "$appbin" ]; then
     profilename="${HOME}/.config/firejail/${APP_NAME}.profile"
     [ -s "$profilename" ] || profilename="/etc/firejail/${APP_NAME}.profile"
     [ -s "$profilename" ] || profilename='/etc/firejail/default.profile'
-#    firejail --appimage --private=~/Applications/privatehome/"$APP_NAME" --profile="$profilename" "$appbin"
-    firejail --appimage  --profile="$profilename" "$appbin"
+    if [ "$PRIVATE_HOME" = "yes" ]; then
+      firejail --appimage --private=~/Applications/privatehome/"$APP_NAME" --profile="$profilename" "$appbin"
+    else
+      firejail --appimage  --profile="$profilename" "$appbin"
+    fi
   else
     "$appbin"
   fi
--- .bin/welle.io
+++ .bin/welle.io
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 export APP_NAME="welle.io"
+export PRIVATE_HOME="yes"
 start_appimage.sh
 
