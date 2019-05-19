7f1fc63561eb12b0fcd262308f5659136166a016
alex <alex@chrome>
2019-05-19 22:04:25 +0200

    CHANGED: vifm loaded with the layout

--- .i3/load_layout.sh
+++ .i3/load_layout.sh
@@ -9,5 +9,5 @@ i3-msg "workspace 1; append_layout ~/.i3/workspace2.json"
 (x-terminal-emulator -title TerminalTailSyslog -e sh -c 'env PROMPT_COMMAND="unset PROMPT_COMMAND; tail -F /var/log/syslog" bash') &
 (x-terminal-emulator -title TerminalDmesg -e sh -c 'env PROMPT_COMMAND="unset PROMPT_COMMAND; dmesg -wt" bash') &
 (x-terminal-emulator -title TerminalShownetstat -e sh -c 'env PROMPT_COMMAND="unset PROMPT_COMMAND; shownetstat" bash') &
-(x-terminal-emulator -title TerminalWork -e 'bash') &
+(x-terminal-emulator -title TerminalWork -e bash -c 'env PROMPT_COMMAND="unset PROMPT_COMMAND; vifm" bash') &
 
