89c62a97e5542501126ec0145fb0317b3e5a0ac4
alex <alex@chrome>
2020-01-14 20:35:23 +0100

    ADDED: private_home_dir var

--- .bin/start_appimage.sh
+++ .bin/start_appimage.sh
@@ -5,12 +5,14 @@
 appbin=$(find ~/Applications/ -iname "$APP_NAME"'*' -exec sh -c '[ -x "{}" -a ! -d "{}" ] && echo "{}"' \;)
 if [ -n "$appbin" ]; then
   if [ -n "$(which firejail)" ]; then
-    [ -d ~/Applications/privatehome/"$APP_NAME" ] || mkdir -p ~/Applications/privatehome/"$APP_NAME"
     profilename="${HOME}/.config/firejail/${APP_NAME}.profile"
     [ -s "$profilename" ] || profilename="/etc/firejail/${APP_NAME}.profile"
     [ -s "$profilename" ] || profilename='/etc/firejail/default.profile'
     if [ "$PRIVATE_HOME" = "yes" ]; then
-      firejail --private=~/Applications/privatehome/"$APP_NAME" --profile="$profilename" --appimage "$appbin" $APP_ARGS
+      privatehomedir="$HOME/Applications/privatehome/$APP_NAME"
+      [ -n "$PRIVATE_HOME_DIR" ] && privatehomedir="$PRIVATE_HOME_DIR/$APP_NAME"
+      [ -d  "$privatehomedir" ] || mkdir -p "$privatehomedir"
+      firejail --private="$privatehomedir" --profile="$profilename" --appimage "$appbin" $APP_ARGS
     else
       firejail --profile="$profilename" --appimage "$appbin" $APP_ARGS
     fi
