18843d49a2c75f4a3113c30306094a837553382d
flux242 <flux242@gmail.com>
2020-01-19 20:52:30 +0100

    REMOVED: gutentags vim plugin

--- .bin/vim_install_plugins.sh
+++ .bin/vim_install_plugins.sh
@@ -8,4 +8,4 @@ git clone --depth=1 https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/ner
 git clone --depth=1 https://github.com/plasticboy/vim-markdown.git ~/.vim/bundle/vim-markdown.git
 git clone --depth=1 https://github.com/edkolev/promptline.vim ~/.vim/bundle/promptline.vim
 git clone --depth=1 https://github.com/skywind3000/asyncrun.vim.git ~/.vim/bundle/asyncrun.vim
-git clone --depth=1 https://github.com/ludovicchabant/vim-gutentags.git ~/.vim/bundle/vim-gutentags.git
+#git clone --depth=1 https://github.com/ludovicchabant/vim-gutentags.git ~/.vim/bundle/vim-gutentags.git
