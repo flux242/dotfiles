bcc721bb822ccfac5f5784bb8976fc702c6ec9dd
flux242 <flux242@gmail.com>
2020-08-30 03:42:40 +0200

    ADDED: vim-webdevicons bundle

--- .bin/vim_install_plugins.sh
+++ .bin/vim_install_plugins.sh
@@ -9,3 +9,4 @@ git clone --depth=1 https://github.com/plasticboy/vim-markdown.git ~/.vim/bundle
 git clone --depth=1 https://github.com/edkolev/promptline.vim ~/.vim/bundle/promptline.vim
 git clone --depth=1 https://github.com/skywind3000/asyncrun.vim.git ~/.vim/bundle/asyncrun.vim
 #git clone --depth=1 https://github.com/ludovicchabant/vim-gutentags.git ~/.vim/bundle/vim-gutentags.git
+git clone --depth=1 https://github.com/cj/vim-webdevicons.git ~/.vim/bundle/vim-webdevicons
