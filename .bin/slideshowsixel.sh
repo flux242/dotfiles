1386ec719bf53d467a6938241d49e3c1c8d90087
flux242 <flux242@gmail.com>
2021-02-17 19:38:36 +0100

    CHANGED: added command line options parsing

--- .bin/slideshowsixel.sh
+++ .bin/slideshowsixel.sh
@@ -12,10 +12,22 @@
 #       I do not know what's the relation between the font size and
 #       the text cell size, so I simply find proper values empirically
 
-do_zoom=1 # do zoom if a pictures is smaller than the terminal window size
+do_zoom="" # do zoom if a pictures is smaller than the terminal window size
 delay=3   # seconds between pictures
 font_width=11
 font_height=23
+
+while getopts ":d:w:h:z" opt; do
+  case $opt in
+      d) delay=$OPTARG ;;
+      w) font_width=$OPTARG ;;
+      h) font_heght=$OPTARG ;;
+      z) do_zoom=1 ;;
+     \?) echo "Invalid option: $OPTARG" >/dev/sdterr ;;
+      :) echo "Option $OPTARG requires an argument" >/dev/sdterr ;;
+  esac
+done
+shift $((OPTIND-1))
 
 PIC_DIR="$1"
 [ -d "$PIC_DIR" ] || PIC_DIR="$HOME/media/test/pics"
@@ -56,7 +68,7 @@ for pic in "$PIC_DIR"/*.jpg; do
     canva_height=$CANVA_HEIGHT
   fi
 
-  echo "set term sixelgd size $canva_width,$canva_height animate truecolor; \
+  echo "set term sixelgd size $canva_width,$canva_height animate truecolor crop enhanced; \
         set lmargin screen 0;set rmargin screen 1; \
         set tmargin screen 0;set bmargin screen 1; \
         unset border; unset tics;unset key; \
