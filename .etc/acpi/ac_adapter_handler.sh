9173f5885cfd8d899daa694fcbe2af97956b40dd
flux242 <flux242@gmail.com>
2022-03-12 11:20:58 +0100

    ADDED: acpi hook to disable compton on battery

---
+++ .etc/acpi/ac_adapter_handler.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# acpi handler for AC adapter events
+
+case "$2" in
+  AC*|AD*)
+     case "$4" in
+       00000000)
+	   killall -q compton
+       ;;
+       00000001)
+           /usr/bin/pgrep '^compton' &>/dev/null || {
+               pid=$(/usr/bin/pgrep 'xfsettingsd') # pid of some process that is always started
+               user=$(/usr/bin/awk 'BEGIN{RS="\x00";FS="="}/(USER)/{print $2}' "/proc/$pid/environ")
+               export $(/usr/bin/awk 'BEGIN{RS="\x00"}/(DISPLAY|HOME)/{print $0}' "/proc/$pid/environ")
+	       cat /proc/$pid/environ > /tmp/acpi-$user-$pid.txt
+#               su $user -c '/usr/bin/compton -b'
+               sudo -u $user /usr/bin/compton -b
+           }
+       ;;
+     esac
+     ;;
+  *) logger "ACPI action undefined: $2"
+     ;;
+esac
+
+exit 0
