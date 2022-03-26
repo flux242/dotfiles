4cd3c02039038c011d3c62e7907362a91f3f6e2f
flux242 <flux242@gmail.com>
2022-03-26 19:17:34 +0100

    ADDED: createsymlinks.sh script for initial system setup instead of the dotfiles management tool

---
+++ bin/createsymlinks.sh
@@ -0,0 +1,23 @@
+#/bin/sh
+
+# this script is only run once after system installation and pulling my
+# dotfiles from the github. It creates necessary symbolic links
+
+createSymbolicLink()
+{
+  [ -n "$1" ] || return
+
+  [ -h "$HOME/$1" ] || {
+    # -h means file exists and it's a symbolic link
+    # remove the dir if it exists but not a symbolic link
+    # rm -f "$HOME/$1" TODO add prompt before removing
+    echo "creating symbolic link for: $1"
+    ln -s "$HOME/dotfiles/$1" "$1"
+  }
+
+}
+
+links=("bin" ".vim" ".vimrc" ".i3" ".highlight" ".grc" ".nticon.conf" ".dircolors" ".compton.conf" ".config/vifm" ".config/mpv" ".config/neofetch/config.conf")
+
+for link in ${links[@]}; do createSymbolicLink $link; done
+
