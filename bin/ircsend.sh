19fd1e87462c3c4a2d2ef5eca21081a3d36479e0
flux242 <flux242@gmail.com>
2022-04-17 09:10:16 +0200

    CHANGED: saves and restores joined channels

--- bin/ircsend.sh
+++ bin/ircsend.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 historyfile="$HOME/.ircsendhistory"
+channelsfile="$HOME/.ircchannels"
+
 HISTFILE="$historyfile"
 HISTCONTROL=ignoreboth
 
@@ -8,8 +10,15 @@ HISTCONTROL=ignoreboth
   echo "Usage: $0 tmp_channel_file_created_by_irclogbot_script"
   exit 1
 }
-channel=$(awk '/JOIN/{printf("%s",$2)}' "$1")
-nick=$(awk '/USER/{printf("%s",$2)}' "$1")
+channel=$(awk '/JOIN/{printf("%s",$2);exit(0)}' "$1")
+nick=$(awk '/USER/{printf("%s",$2);exit(0)}' "$1")
+
+echo "CAP REQ :echo-message" >> "$1"
+cat "$channelsfile" | while read channel; do
+  [[ "$channel" =~ ^#[^[:space:]]+ ]] && {
+    printf "JOIN $channel\n" >> "$1"
+  }
+done
 
 history -r
 while read -erp "$channel> " message; do
@@ -17,7 +26,8 @@ while read -erp "$channel> " message; do
     case "$message" in
       /nick*) nick="${message#* }"; echo "${message:1}" >> "$1" ;;
       /ident*) nickpass="${message#* }"; echo "PRIVMSG NickServ :IDENTIFY $nickpass" >> "$1" ;;
-      /join*) grep -qoP '\s+\K(#[^\s]+)$' <<< "$message" && channel="${message#* }" && echo "${message:1}" >> "$1" ;;
+      /join*) grep -qoP '\s+\K(#[^\s]+)$' <<< "$message" && channel="${message##* }" && sed -i "/$channel/d" "$channelsfile" && echo "$channel" >> "$channelsfile" && echo "${message:1}" >> "$1" ;;
+      /part*) grep -qoP '\s+\K(#[^\s]+)$' <<< "$message" && channel="${message##* }" && sed -i "/$channel/d" "$channelsfile" && echo "${message:1}" >> "$1" ;; 
       /whoami*) echo "whois $nick" >> "$1" ;;
       /msg*) msg="${message#* }";name="${msg%% *}";rest="${msg#* }";echo "PRIVMSG $name :$rest" >> "$1" ;;
       /me*) printf "PRIVMSG $channel :\x01ACTION ${message#* }\x01\n" >> "$1" ;;
