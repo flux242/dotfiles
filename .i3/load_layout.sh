98aa80d1089e2d30bb5bb8d37f2612fe0b7ca86a
flux242 <flux242@gmail.com>
2022-05-18 06:33:50 +0200

    CHANGED: dmesg requires sudo now

--- .i3/load_layout.sh
+++ .i3/load_layout.sh
@@ -7,7 +7,7 @@
 i3-msg "workspace 1; append_layout ~/.i3/workspace2.json"
 
 (x-terminal-emulator -title TerminalTailSyslog -e sh -c 'env PROMPT_COMMAND="unset PROMPT_COMMAND; tail -F /var/log/syslog" bash') &
-(x-terminal-emulator -title TerminalDmesg -e sh -c 'env PROMPT_COMMAND="unset PROMPT_COMMAND; dmesg -wt" bash') &
+(x-terminal-emulator -title TerminalDmesg -e sh -c 'env PROMPT_COMMAND="unset PROMPT_COMMAND; sudo dmesg -wt" bash') &
 (x-terminal-emulator -title TerminalShownetstat -e sh -c 'env PROMPT_COMMAND="unset PROMPT_COMMAND; shownetstat" bash') &
 (x-terminal-emulator -title TerminalWork -e bash -c 'env PROMPT_COMMAND="unset PROMPT_COMMAND; vifm" bash') &
 
