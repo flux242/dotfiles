a31bf8f80c0a482a661d3565acb7a6b1059769ae
flux242 <flux242@gmail.com>
2022-05-18 07:26:13 +0200

    CHANGED: https instead of git

--- bin/vim_install_plugins.sh
+++ bin/vim_install_plugins.sh
@@ -1,9 +1,9 @@
 git clone --depth=1 https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
 git clone --depth=1 https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
-git clone --depth=1 git://github.com/airblade/vim-gitgutter.git ~/.vim/bundle/vim-gitgutter
+git clone --depth=1 https://github.com/airblade/vim-gitgutter.git ~/.vim/bundle/vim-gitgutter
 #git clone svn://github.com/ajbetteridge/vim-svngutter.svn ~/.vim/bundle/vim-svngutter
 git clone --depth=1 https://github.com/vim-scripts/vim-svngutter.git ~/.vim/bundle/vim-svngutter
-git clone --depth=1 git://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-figutive
+git clone --depth=1 https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-figutive
 git clone --depth=1 https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 git clone --depth=1 https://github.com/plasticboy/vim-markdown.git ~/.vim/bundle/vim-markdown.git
 git clone --depth=1 https://github.com/edkolev/promptline.vim ~/.vim/bundle/promptline.vim
