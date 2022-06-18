7cd73f20a31625b09c4380e168941650a0283845
flux242 <flux242@gmail.com>
2022-06-18 07:39:48 +0200

    COSMETIC: cleanup

--- bin/webcams.sh
+++ bin/webcams.sh
@@ -3,23 +3,17 @@
 
 SIZE=640
 
-
-#declare -A sizes
-sizes=( 180 240 400 640 720 1200 1280 1600 1920 )
+declare -a sizes=( 180 240 400 640 720 1200 1280 1600 1920 )
 
 if [[ -n "$1" ]]; then
   if [[ "${sizes[@]}" =~ "$1" ]]; then
     SIZE=$1
   else
-    echo "Size $1 is not supported. Supported sizes are:" > /dev/stderr
-    for i in "${sizes[@]}"; do printf "%s " $i; done > /dev/stderr
-    printf "\n"
+    printf "Size $1 is not supported. Supported sizes are: %s\n" "${sizes[*]}" > /dev/stderr
     exit 1
   fi
 fi
 
-
-#wget -qO- https://sdo.gsfc.nasa.gov/assets/img/latest/latest_512_0171.jpg | img2sixel
 wget -qO- https://www.foto-webcam.eu/webcam/rosenheim/current/$SIZE.jpg | img2sixel
 wget -qO- https://www.addicted-sports.com/fileadmin/webcam/chiemsee/current/$SIZE.jpg | img2sixel
 wget -qO- https://www.addicted-sports.com/fileadmin/webcam/torbole/current/$SIZE.jpg | img2sixel
