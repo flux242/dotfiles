030c62fc0401373d5ab0852b20a43667e870e820
flux242 <flux242@gmail.com>
2023-02-16 13:25:49 +0100

    CHANGED: added redshift symlink

--- bin/createsymlinks.sh
+++ bin/createsymlinks.sh
@@ -17,7 +17,7 @@ createSymbolicLink()
 
 }
 
-links=(".screenrc" ".bash_aliases" "bin" ".vim" ".vimrc" ".i3" ".highlight" ".grc" ".nticon.conf" ".dircolors" ".compton.conf" ".config/vifm" ".config/mpv" ".config/neofetch" ".config/pnmixer" ".gnokiirc")
+links=(".screenrc" ".bash_aliases" "bin" ".vim" ".vimrc" ".i3" ".highlight" ".grc" ".nticon.conf" ".dircolors" ".compton.conf" ".config/vifm" ".config/mpv" ".config/neofetch" ".config/pnmixer" ".config/redshift" ".gnokiirc")
 
 for link in ${links[@]}; do createSymbolicLink $link; done
 
