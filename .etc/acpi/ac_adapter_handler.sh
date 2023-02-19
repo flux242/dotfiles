eb1de1ff7697d38289fe8a930d4e58c58f5ad0df
flux242 <flux242@gmail.com>
2023-02-19 14:05:35 +0100

    CHANGED: compositor switched to picom

--- .etc/acpi/ac_adapter_handler.sh
+++ .etc/acpi/ac_adapter_handler.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 # acpi handler for AC adapter events
 
+compositor="picom"
+
 case "$2" in
   AC*|AD*)
      case "$4" in
        00000000)
-	   killall -q compton
+           killall -q "$compositor"
        ;;
        00000001)
-           /usr/bin/pgrep '^compton' &>/dev/null || {
+           /usr/bin/pgrep "^${compositor}" &>/dev/null || {
                pid=$(/usr/bin/pgrep 'xfsettingsd') # pid of some process that is always started
                user=$(/usr/bin/awk 'BEGIN{RS="\x00";FS="="}/(USER)/{print $2}' "/proc/$pid/environ")
                export $(/usr/bin/awk 'BEGIN{RS="\x00"}/(DISPLAY|HOME)/{print $0}' "/proc/$pid/environ")
 #               su $user -c '/usr/bin/compton -b'
-               sudo -u $user /usr/bin/compton -b
+               sudo -u $user /usr/bin/$compositor -b
            }
        ;;
      esac
