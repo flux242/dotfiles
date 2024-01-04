ab429dc7c648bd914bcdaa929617f7e0d6efd5ac
flux242 <flux242@gmail.com>
2024-01-04 10:33:32 +0100

    BUGIFX: proper year is used

--- bin/moon.sh
+++ bin/moon.sh
@@ -3,7 +3,7 @@
 # first of jan picture is 0013, then 24 is added for each next day
 MAGIC_CONST=13
 
-hours=$((($(date +%s)-$(date +%s -d '2023/1/1'))/(24*60*60)*24+$MAGIC_CONST))
+hours=$((($(date +%s)-$(date +%s -d "$(date +%Y)/1/1"))/(24*60*60)*24+$MAGIC_CONST))
 
 wget -qO- "https://moon.nasa.gov/mvg.$(date +%Y)/$(printf '%04d' $hours).jpg" | img2sixel
 
