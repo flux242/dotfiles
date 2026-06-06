dc8a4c9c669c76398b21a9af9b508e1261c79dd7
flux242 <flux242@gmail.com>
2026-06-06 07:36:45 +0200

    updated moon script

--- bin/moon.sh
+++ bin/moon.sh
@@ -5,5 +5,8 @@ MAGIC_CONST=13
 
 hours=$((($(date +%s)-$(date +%s -d "$(date +%Y)/1/1"))/(24*60*60)*24+$MAGIC_CONST))
 
-wget -qO- "https://moon.nasa.gov/mvg.$(date +%Y)/$(printf '%04d' $hours).jpg" | img2sixel
+# this stopped working in 2026
+#wget -qO- "https://moon.nasa.gov/mvg.$(date +%Y)/$(printf '%04d' $hours).jpg" | img2sixel
 
+#https://svs.gsfc.nasa.gov/vis/a000000/a005500/a005587/frames/730x730_1x1_30p/moon.0751.jpg
+wget -qO- "https://svs.gsfc.nasa.gov/vis/a000000/a005500/a005587/frames/730x730_1x1_30p/moon.$(printf '%04d' $hours).jpg" | img2sixel
